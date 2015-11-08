347c1da261747dac6787edc97225bc0a5bfae72a
jocelyn <jocelyn@insensi.com>
2015-11-08 15:29:00 -0500

    Add first nrf51 project

---
+++ led_blinking/src/main.c
@@ -0,0 +1,12 @@
+//
+//  main.c
+//  led_blinking
+//
+//  Created by Jocelyn MASSEROT on 11/08/2015.
+//  Copyright (c) 2015 Jocelyn MASSEROT. All rights reserved.
+//
+
+
+int main(void){
+    return 0;
+}
